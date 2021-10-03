02d82966a9505068da618caf528c80d6d7371b28
niharika rastogi <rastoginiha2002@gmail.com>
2021-10-03 22:04:39 +0530

    animated few elements

--- script/js.js
+++ script/js.js
@@ -1,16 +1,113 @@
 function animation()
 {
+    setTimeout(animateCloud1,100);
+    setTimeout(animateCloud2,100);
+    setTimeout(animateCloud3,100);
+    setTimeout(animateGhost1,100);
+    setTimeout(animateGhost2,100);
+    setTimeout(animateGhost3,100);
+}
+function animateCloud1()
+{
     const cloud1 = document.getElementById("cloud1");   
-    var x=document.getElementById("cloud1").style.left;
-    var y=document.getElementById("cloud1").style.top;
+    var rect=cloud1.getBoundingClientRect();
+    var y=rect.top;
+    var x=rect.left;
     var angle=0;
-    var id = setInterval(animate, 1000);
+    var id = setInterval(animate, 4);
     function animate(){
-        if(x==0)
-            clearInterval(id);
+        if(x==-rect.width)
+            x=window.innerWidth;
         else{
             x--;
-            cloud1.style.left = pos + "px";
+            y=Math.round(20 * Math.sin(angle));
+            angle += .01;
+            document.getElementById("cloud1").style.left=x+"px";
+            document.getElementById("cloud1").style.top=y+"px";
         }
     }
+}
+
+function animateCloud2()
+{
+    const cloud2 = document.getElementById("cloud2");   
+    var rect=cloud2.getBoundingClientRect();
+    var y=rect.top;
+    var x=rect.left;
+    var angle=0;
+    var id = setInterval(animate, 1);
+    function animate(){
+        if(x==window.innerWidth)
+            x=-rect.width;
+        else{
+            x++;
+            y=rect.top+Math.round(20 * Math.sin(angle));
+            angle += .01;
+            document.getElementById("cloud2").style.left=x+"px";
+            document.getElementById("cloud2").style.top=y+"px";
+        }
+    }    
+}
+
+function animateCloud3()
+{
+    const cloud3 = document.getElementById("cloud3");   
+    var rect=cloud3.getBoundingClientRect();
+    var y=rect.top;
+    var x=rect.left;
+    var angle=0;
+    var id = setInterval(animate, 1);
+    function animate(){
+        if(x==-rect.width)
+            x=window.innerWidth;
+        else{
+            x--;
+            y=rect.top+Math.round(20 * Math.sin(angle));
+            angle += .01;
+            document.getElementById("cloud3").style.left=x+"px";
+            document.getElementById("cloud3").style.top=y+"px";
+        }
+    }    
+}
+
+function animateGhost1()
+{
+    const ghost1 = document.getElementById("ghost1");   
+    var rect=ghost1.getBoundingClientRect();
+    var x=rect.left;
+    var angle=0;
+    var id = setInterval(animate, 1);
+    function animate(){
+        x=rect.left+Math.round(20 * Math.sin(angle));
+        angle += .02;
+        document.getElementById("ghost1").style.left=x+"px";
+    }    
+}
+
+function animateGhost2()
+{
+    const ghost2 = document.getElementById("ghost2");   
+    var rect=ghost2.getBoundingClientRect();
+    var x=rect.left;
+    var angle=0;
+    var id = setInterval(animate, 1);
+    function animate(){
+        x=rect.left+Math.round(20 * Math.sin(angle));
+        angle += .02;
+        document.getElementById("ghost2").style.left=x+"px";
+    }    
+}
+
+function animateGhost3()
+{
+    const ghost3 = document.getElementById("ghost3");   
+    var rect=ghost3.getBoundingClientRect();
+    var x=rect.left;
+    var angle=0;
+    var id = setInterval(animate, 1);
+    function animate(){
+        x=rect.left+Math.round(20 * Math.sin(angle));
+        angle += .02;
+        document.getElementById("ghost3").style.left=x+"px";
+    }    
 }
